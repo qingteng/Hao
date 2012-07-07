c39658d1cbaa2f3458260a0ed647b48379942d2e
qingteng <szkyaojiayou@163.com>
2012-07-07 20:57:53 +0800

    Initial Commit

---
+++ SSSSTests/SSSSTests.h
@@ -0,0 +1,13 @@
+//
+//  SSSSTests.h
+//  SSSSTests
+//
+//  Created by bin huang on 12-7-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSSSTests : SenTestCase
+
+@end
---
+++ SSSSTests/SSSSTests.m
@@ -0,0 +1,32 @@
+//
+//  SSSSTests.m
+//  SSSSTests
+//
+//  Created by bin huang on 12-7-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SSSSTests.h"
+
+@implementation SSSSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SSSSTests");
+}
+
+@end
