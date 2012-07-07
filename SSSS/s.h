068bbff69585641a4a89961d4e7b81a6c556c8ff
qingteng <szkyaojiayou@163.com>
2012-07-07 21:11:18 +0800

    s

---
+++ SSSS/s.h
@@ -0,0 +1,13 @@
+//
+//  s.h
+//  SSSS
+//
+//  Created by bin huang on 12-7-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface s : UIView
+
+@end
---
+++ SSSS/s.m
@@ -0,0 +1,31 @@
+//
+//  s.m
+//  SSSS
+//
+//  Created by bin huang on 12-7-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "s.h"
+
+@implementation s
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
